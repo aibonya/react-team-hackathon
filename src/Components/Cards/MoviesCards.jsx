04c57224a8fc29d87ac75d6f80f61fe5f7d15471
aibonya <0502222323@mail.ru>
2022-06-14 21:44:20 +0600

    added addMovies and moviesCard

--- src/Components/Cards/MoviesCards.jsx
+++ src/Components/Cards/MoviesCards.jsx
@@ -19,18 +19,9 @@ export default function MoviesCards({ item }) {
                 <CardMedia
                   component="img"
                   height="280"
-                  image="https://avatars.mds.yandex.net/get-kinopoisk-image/1777765/4ae92a3a-b7fd-4d21-923b-c55c4f2aac7c/600x900"
+                  image={item.image}
                   alt="green bugatti"
                 />
-                {/* <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Название фильма
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Aliquam, magni?
-                  </Typography>
-                </CardContent> */}
               </CardActionArea>
             </Card>
           }
